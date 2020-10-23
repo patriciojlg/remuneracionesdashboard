d2763d2198e475cbf235d498f35d16d7a9c4c2b9
GlopGlup <patricio.jlg@gmail.com>
2020-10-23 09:38:24 -0300

    Base deploy react para github pages

---
+++ precache-manifest.a5bbe21a1ad8ec2f0aedd0d5a22fd3b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4bed3b6a073ed7e475b40194872a58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "340f22daa6bf035fc50e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "afed0fede61fdf4e3228",
+    "url": "/static/js/2.a93af918.chunk.js"
+  },
+  {
+    "revision": "c6b51bd476e66fa8e274f35df5fab356",
+    "url": "/static/js/2.a93af918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340f22daa6bf035fc50e",
+    "url": "/static/js/main.122081f8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
